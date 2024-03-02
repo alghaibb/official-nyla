2242a7a26c8ab0fc74b70cb1381e1ffc61b23dfe
Moudi <mjaderi97@gmail.com>
2024-03-02 11:43:57 +1100

    Refactored payload header to suit clients needs

--- src/app/_components/Header/Nav/index.tsx
+++ src/app/_components/Header/Nav/index.tsx
@@ -1,11 +1,9 @@
 'use client'
 
 import React from 'react'
-import Link from 'next/link'
 
-import { Header as HeaderType, User } from '../../../../payload/payload-types'
+import { Header as HeaderType } from '../../../../payload/payload-types'
 import { useAuth } from '../../../_providers/Auth'
-import { CartLink } from '../../CartLink'
 import { CMSLink } from '../../Link'
 
 import classes from './index.module.scss'
@@ -15,27 +13,15 @@ export const HeaderNav: React.FC<{ header: HeaderType }> = ({ header }) => {
   const { user } = useAuth()
 
   return (
-    <nav
-      className={[
-        classes.nav,
-        // fade the nav in on user load to avoid flash of content and layout shift
-        // Vercel also does this in their own website header, see https://vercel.com
-        user === undefined && classes.hide,
-      ]
-        .filter(Boolean)
-        .join(' ')}
-    >
-      {navItems.map(({ link }, i) => {
-        return <CMSLink key={i} {...link} appearance="none" />
+    <nav className={[classes.nav, user === undefined && classes.hide].filter(Boolean).join(' ')}>
+      {navItems.map((navItem, i) => {
+        const key = navItem.link.href || `nav-item-${i}`
+        return (
+          <div key={key} className={classes.navLinks}>
+            <CMSLink {...navItem.link} />
+          </div>
+        )
       })}
-      <CartLink />
-      {user && <Link href="/account">Account</Link>}
-      {!user && (
-        <React.Fragment>
-          <Link href="/login">Login</Link>
-          <Link href="/create-account">Create Account</Link>
-        </React.Fragment>
-      )}
     </nav>
   )
 }
