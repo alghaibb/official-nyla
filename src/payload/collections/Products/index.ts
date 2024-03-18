bfa1e13513f5b3e6b652f675c5e1098c0eeec66d
Moudi <mjaderi97@gmail.com>
2024-03-18 18:23:26 +1100

    Refactored products page to suit client needs & added related products to product pages

--- src/payload/collections/Products/index.ts
+++ src/payload/collections/Products/index.ts
@@ -82,7 +82,6 @@ const Products: CollectionConfig = {
             {
               name: 'layout',
               type: 'blocks',
-              required: true,
               blocks: [CallToAction, Content, MediaBlock, Archive],
             },
           ],
